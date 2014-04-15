d710d628bd79a655b9d308a90f1dc6cc12ca19cf
Casper Hollingberry <hollingberry7@gmail.com>
2014-04-15 19:04:48 -0300

    add waning/waxing data

--- index.js
+++ index.js
@@ -7,10 +7,32 @@ var suncalc = require('suncalc'),
     geolocation_url = 'https://maps.googleapis.com/maps/api/browserlocation/json?browser=chromium&sensor=true';
 
 request(geolocation_url, function (err, res, body) {
+    if (err) {
+        console.log(chalk.red('Geolocation request failed.\n' + err));
+        return;
+    }
     var geolocation_data = JSON.parse(body);
-    var suncalc_data = suncalc.getTimes(new Date(), geolocation_data.location.lat, geolocation_data.location.lng);
-    console.log(chalk.yellow('Dawn: ') + moment(suncalc_data.dawn).calendar());
-    console.log(chalk.yellow('Sunrise: ') + moment(suncalc_data.sunrise).calendar());
-    console.log(chalk.yellow('Sunset: ') + moment(suncalc_data.sunset).calendar());
-    console.log(chalk.yellow('Dusk: ') + moment(suncalc_data.dusk).calendar());
+    display(geolocation_data.location);
 });
+
+function display (coords) {
+    var sun_times = suncalc.getTimes(new Date(), coords.lat, coords.lng);
+    var moon_illumination = suncalc.getMoonIllumination(new Date());
+    output('Dawn', sun_times.dawn);
+    output('Sunrise', sun_times.sunrise);
+    output('Sunset', sun_times.sunset);
+    output('Dusk', sun_times.dusk);
+    console.log(chalk.blue('Moon Phase: ') + phase(moon_illumination.fraction, moon_illumination.angle));
+}
+
+function output (name, date) {
+    console.log(chalk.yellow(name + ': ') + moment(date).calendar());
+}
+
+function phase (fraction, angle) {
+    if (angle > 0) {
+        return 'Waning';
+    } else if (angle < 0) {
+        return 'Waxing ';
+    }
+}
